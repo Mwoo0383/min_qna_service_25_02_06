fd4c58b69fdc372f6b33f49e993ac9f62809de30
Minwoo <cap019@naver.com>
2025-02-07 00:51:40 +0900

    @OneToMany 필드 추가

--- src/main/java/com/minu/qna_service/boundedContext/question/Question.java
+++ src/main/java/com/minu/qna_service/boundedContext/question/Question.java
@@ -1,11 +1,13 @@
 package com.minu.qna_service.boundedContext.question;
 
+import com.minu.qna_service.boundedContext.answer.Answer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,5 +27,6 @@ public class Question {
 
     private LocalDateTime createDate;
 
-
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
