084f9735781177af575113aebf76e45890e61f0c
Minwoo <cap019@naver.com>
2025-02-10 21:27:42 +0900

    SQL SELECT

--- src/main/java/com/minu/qna_service/boundedContext/repository/QuestionRepository.java
+++ src/main/java/com/minu/qna_service/boundedContext/repository/QuestionRepository.java
@@ -3,5 +3,12 @@ package com.minu.qna_service.boundedContext.repository;
 import com.minu.qna_service.boundedContext.question.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/minu/qna_service/QnaServiceApplicationTests.java
+++ src/test/java/com/minu/qna_service/QnaServiceApplicationTests.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class QnaServiceApplicationTests {
@@ -19,8 +23,8 @@ class QnaServiceApplicationTests {
     @DisplayName("데이터 저장하기")
     void t001() {
         Question q1 = new Question();
-        q1.setSubject("sbs가 무엇인가요?");
-        q1.setContent("sbs에 대해서 알고 싶습니다.");
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
         q1.setCreateDate(LocalDateTime.now());
         questionRepository.save(q1);
 
@@ -31,4 +35,45 @@ class QnaServiceApplicationTests {
         questionRepository.save(q2);
     }
 
+    @Test
+    @DisplayName("findAll")
+    void t002() {
+        List<Question> all = questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+    @Test
+    @DisplayName("findById")
+    void t003() {
+        Optional<Question> oq = questionRepository.findById(1);
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
+
+    @Test
+    @DisplayName("findBySubject")
+    void t004() {
+         Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+         assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectAndContent")
+    void t005() {
+        Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectLike")
+    void t006() {
+        List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }
